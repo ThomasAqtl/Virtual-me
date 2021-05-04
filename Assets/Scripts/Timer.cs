e7dcf088ad58ba2b9e47ca8da45371e2e53932b1
ThomasAqtl <36239513+ThomasAqtl@users.noreply.github.com>
2021-05-05 01:00:49 +0200

    Test campfire

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -11,9 +11,9 @@ public class Timer : MonoBehaviour
     [Header("Default starting time")]
     [Range(0, 23)]
     public int hour;
-    [Range(0,59)]
+    [Range(0, 59)]
     public int minute;
-    [Range(0,59)]
+    [Range(0, 59)]
     public float second;
 
     [Header("Skybox color settings")]
@@ -27,37 +27,41 @@ public class Timer : MonoBehaviour
     public Color skyboxBottomColorDark;
 
     // format _time = Vector3 (hours, minutes, seconds)
-   private Vector3 _time;
+    private Vector3 _time;
 
-   private const float _anglePerHour = 360.0f/24.0f;
-   private const float _anglePerMinute = _anglePerHour/60.0f;
-   private const float _anglePerSecond = _anglePerMinute/60.0f;
-   private const float _secondsPerDay = 86400.0f;
-   private Color _skyboxColor1;
-   private Color _skyboxColor2;
+    // const
+    private const float _anglePerHour = 360.0f / 24.0f;
+    private const float _anglePerMinute = _anglePerHour / 60.0f;
+    private const float _anglePerSecond = _anglePerMinute / 60.0f;
+    private const float _secondsPerDay = 86400.0f;
 
-    private void Awake() {
+    private void Awake()
+    {
         _time = new Vector3(hour, minute, second);
-        _skyboxColor1 = RenderSettings.skybox.GetColor("_SkyGradientTop");
-        _skyboxColor2 = RenderSettings.skybox.GetColor("_SkyGradientBottom");
     }
-    private void Update() {
+
+    private void Update()
+    {
         if (Application.isPlaying) {
             UpdateTime();
         } else {
             _time = new Vector3(hour, minute, second);
         }
+
         UpdateSunPos();
         UpdateSkybox();
-    }    
+    }
     private void UpdateTime() {
         _time.z += Time.deltaTime * timeFactor;
-        
-        if (_time.z >= 60.0f) {
+
+        if (_time.z >= 60.0f)
+        {
             _time.z = 0.0f;
-            if (_time.y >= 60.0f) {
+            if (_time.y >= 60.0f)
+            {
                 _time.y = 0.0f;
-                if (_time.x >= 24.0f) {
+                if (_time.x >= 24.0f)
+                {
                     _time.x = 0.0f;
                 } else {
                     _time.x += 1;
@@ -69,7 +73,6 @@ public class Timer : MonoBehaviour
     }
 
     private void UpdateSunPos() {
-
         // angle = 90° => midday, angle = 270° => noon
         // light is on reset position (0 , 0,  0)
         // We start from noon to compute light angle
@@ -79,29 +82,29 @@ public class Timer : MonoBehaviour
     }
 
     private void UpdateSkybox() {
-        // Skybox color depends on time
+        // Sky color varies with respect to time
+
         float t = _time.x * 3600.0f + _time.y * 60.0f + _time.z;
         float lambda = SetLambda(t);
-        
-        Color c1 = lambda * skyboxTopColorDark + (1-lambda) * skyboxTopColorBright;
+
+        Color c1 = lambda * skyboxTopColorDark + (1 - lambda) * skyboxTopColorBright;
         Color c2 = lambda * skyboxBottomColorDark + (1 - lambda) * skyboxBottomColorBright;
-        
+
         RenderSettings.skybox.SetColor("_SkyGradientTop", c1);
         RenderSettings.skybox.SetColor("_SkyGradientBottom", c2);
-        //print(_skyboxColor1);
     }
 
     public string GetTime() {
 
-        string h = _time.x < 10 ? "0"+_time.x : _time.x.ToString();
-        string m = _time.y < 10 ? "0"+_time.y : _time.y.ToString();
-        string s = _time.z < 10 ? "0" + _time.z : _time.z.ToString("0.");
+        string h = _time.x < 10 ? "0" + _time.x.ToString() : _time.x.ToString();
+        string m = _time.y < 10 ? "0" + _time.y.ToString() : _time.y.ToString();
+        string s = _time.z < 10 ? "0" + _time.z.ToString() : _time.z.ToString("0.");
 
-        string time = "TIME : " + h +":"+ m +":"+ s;
+        string time = "TIME : " + h + ":" + m + ":" + s;
         return time;
     }
 
     private float SetLambda(float t) {
-        return .5f * ( Mathf.Cos( 2 * Mathf.PI / _secondsPerDay * t ) + 1 );
+        return .5f * (Mathf.Cos(2 * Mathf.PI / _secondsPerDay * t) + 1);
     }
-}   
+}
